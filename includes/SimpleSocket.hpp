64da62ffeb4474123144c3860264f1fe4274ae30
Augustin Debureaux <adebureaux@gmail.com>
2022-06-03 18:00:01 +0200

    epoll wip

--- includes/SimpleSocket.hpp
+++ includes/SimpleSocket.hpp
@@ -5,26 +5,34 @@
 #include <cstdio>
 #include <cerrno>
 #include <cstring>
+#include <iostream>
 #include <unistd.h>
+#include <sys/epoll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <iostream>
+
+#define MAX_EVENTS 5
+#define READ_SIZE 10
 
 class SimpleSocket
 {
 	public:
-		SimpleSocket(void);
-		~SimpleSocket(void);
+		SimpleSocket();
+		~SimpleSocket();
 
 		void identify(void);
-		void wait(void);
+		void probe(void);
 		void communicate(void);
+		void perror_exit(std::string err);
 
 	private:
 		int 				_server_fd;
+		int					_socket_fd;
+		int					_epoll_fd;
 		int					_port;
-		int					_socket;
-		struct sockaddr_in _address;
+		struct sockaddr_in	_address;
+		struct epoll_event	_event;
+		struct epoll_event	_events[MAX_EVENTS];
 };
 #endif
--- srcs/SimpleSocket.cpp
+++ srcs/SimpleSocket.cpp
@@ -3,21 +3,20 @@
 SimpleSocket::SimpleSocket(void)
 {
 	if ((_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		perror("cannot create socket");
-		exit(errno);
-	}
+		perror_exit("cannot create socket");
 	int enable = 1;
 	if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1)
-		perror("cannot set socket option 'SO_REUSEADDR'");
+		perror_exit("cannot set socket option 'SO_REUSEADDR'");
 	identify();
-	wait();
+	probe();
 	communicate();
 }
 
 SimpleSocket::~SimpleSocket(void)
 {
-	close(_socket);
+	close(_server_fd);
+	close(_socket_fd);
+	close(_epoll_fd);
 }
 
 void SimpleSocket::identify(void)
@@ -30,35 +29,56 @@ void SimpleSocket::identify(void)
 	_address.sin_addr.s_addr = htonl(INADDR_ANY); 
 	_address.sin_port = htons(_port);
 	if (bind(_server_fd, (struct sockaddr *)&_address, sizeof(_address)) == -1) 
-	{
-		perror("bind failed"); 
-		exit(errno); 
-	}
+		perror_exit("bind failed"); 
 }
 
-void SimpleSocket::wait(void)
+void SimpleSocket::probe(void)
 {
-	/* Change the '3' value after Charles parsing */
-	if (listen(_server_fd, 3) == -1)
-	{
-		perror("In listen");
-		exit(errno);
-	}
+	/* epoll work in progress */
+
+	// if ((_epoll_fd = epoll_create1(0)) == -1)
+	// 	perror_exit("epoll_create1 failed");
+	// _event.events = EPOLLIN;
+	// _event.data.fd = 0;
+	// if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, _server_fd, &_event))
+	// 	perror_exit("adding epoll file descriptor failed");
+	// int running = 1, event_count, i;
+	// size_t bytes_read;
+	// while (running) {
+	// 	printf("\nPolling for input...\n");
+	// 	event_count = epoll_wait(_epoll_fd, _events, MAX_EVENTS, 30000);
+	// 	printf("%d ready events\n", event_count);
+	// 	for (i = 0; i < event_count; i++) {
+	// 		printf("Reading file descriptor '%d' -- ", _events[i].data.fd);
+	// 		bytes_read = read(_events[i].data.fd, read_buffer, READ_SIZE);
+	// 		printf("%zd bytes read.\n", bytes_read);
+	// 		read_buffer[bytes_read] = '\0';
+	// 		printf("Read '%s'\n", read_buffer);
+	// 		if(!strncmp(read_buffer, "stop\n", 5))
+	// 			running = 0;
+	// 	}
+	// }
+
+	if (listen(_server_fd, 1) == -1)
+		perror_exit("listen failed");
 	unsigned int addrlen = sizeof(_address);
-	if ((_socket = accept(_server_fd, (struct sockaddr *)&_address, (socklen_t*)&addrlen)) == -1)
-	{
-		perror("In accept");
-		exit(errno);
-	}
+	if ((_socket_fd = accept(_server_fd, (struct sockaddr *)&_address, (socklen_t*)&addrlen)) == -1)
+		perror_exit("accept failed");
 }
 
 void SimpleSocket::communicate(void)
 {
 	char buffer[1024] = {0};
-	int valread = read(_socket, buffer, 1024);
-	std::cout << buffer;
+	int valread = read(_socket_fd, buffer, 1024);
 	if (valread == -1)
 			exit(1);
-	std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-	write(_socket, hello.c_str(), hello.size());
+	std::string hello = "HTTP/1.1 200 OK";
+	write(_socket_fd, hello.c_str(), hello.size());
+}
+
+void SimpleSocket::perror_exit(std::string err)
+{
+	std::cerr << err << " : " << strerror(errno) << std::endl;
+	this->~SimpleSocket();
+	exit(errno);
 }
