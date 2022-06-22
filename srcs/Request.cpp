e19e6a44ff92cd0b6933d2f204770b4d3e39f8db
mabuchart <matheobuchart66@gmail.com>
2022-06-23 01:14:41 +0200

    GET

--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -14,6 +14,75 @@ std::string get_filename(std::string mess)
     return filename;
 }
 
+std::string get_query(std::string page)
+{
+    std::size_t test = page.find_first_of("?");
+
+    if (test == page.npos)
+        return "";
+
+    return page.substr(test, page.size() - test);
+}
+
+void fill_request_basic(char *msg, int n, Request *r)
+{
+	int i;
+	int j;
+
+	if (n == 1){
+		r->type = "GET";
+		i = 4;
+	}
+	else if (n == 3){
+		r->type = "POST";
+		i = 5;
+	}
+	else {
+		r->type = "DELETE";
+		i = 7;
+	}
+
+	j = i;
+	while (msg[j] != ' ')
+		j++;
+	std::string mess = msg;
+	r->page = mess.substr(i, j - i);
+	r->query = get_query(r->page);
+
+	j += 1;
+	i = j;
+	while (msg[i] != ' ' && msg[i] != '\n' && msg[i] != '\r')
+		i++;
+	std::string http = mess.substr(j, i - j);
+	if (http != "HTTP/1.1"){
+		r->error_type = 505;
+		return ;
+	}
+
+	std::size_t k = mess.find("Host: ");
+	if (k == mess.npos){
+		r->error_type = 400;
+		return ;
+	}
+
+	size_t l = k + 7;
+	while (mess[l] != ':')
+		l++;
+	r->host_address = mess.substr(k, l - k);
+
+	l++;
+	k = l;
+	while (mess[l] != ' ' && mess[l] != '\n' && mess[l] != '\r')
+		l++;
+
+	std::string ip = mess.substr(k, l - k);
+	r->host_ip = atoi(ip.c_str());
+	r->error_type = 200;
+
+	std::cout << r->page << std::endl;
+
+	return ;
+}
 
 void chunked_post(std::string mess, Request *r)
 {
@@ -56,8 +125,14 @@ void chunked_post(std::string mess, Request *r)
 
 void	first_dispatch(char *msg, Request *r)
 {
-	std::cout << msg << std::endl;		///////// a effacer /////////
+	//std::cout << msg << std::endl;		///////// a effacer /////////
 
 	if (r->encoding == "chunked" && r->pure_content != "")
 		chunked_post(msg, r);
+
+	else if (msg[0] == 'G' && msg[1] == 'E' && msg[2] == 'T' && msg[3] == ' ')
+	{
+        fill_request_basic(msg, 1, r);
+        r->status_is_finished = true;
+    }
 }
