1c7c62a9912cf35b3ae950694529f4015bdf1815
adebureaux <adebureaux@gmail.com>
2022-08-01 22:06:23 +0200

    gotta fix test request

--- test/request.py
+++ test/request.py
@@ -1,25 +1,21 @@
- 8.66 KB
-
 import telnetlib
 import base64
+
 import cv2
 
-request =
-"""
+request = """
 POST /test/ HTTP/1.0 From: frog@jmarshall.com
 User-Agent: HTTPTool/1.0
 Content-Type: application/x-www-form-urlencoded
-Content-Length: 32"
-raw_byte = str.encode(raw)
+Content-Length: 32
 """
 
 host = "127.0.0.1"
 port = 8083
-timeout = 100
 
-with telnetlib.Telnet(host, port, timeout) as session:
+with telnetlib.Telnet(host, port, 100) as session:
     session.write(str.encode(request))
-    output = session.read_until(b"done", timeout)
+    output = session.read_until(b"done", 100)
     session.close()
     print(output)
     print("Done")
