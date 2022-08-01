eefbc690e3efa981e7989099bd480f4c53d38c2a
Augustin <adebureaux@gmail.com>
2022-08-01 21:20:19 +0200

    create request tester

---
+++ test/request.py
@@ -0,0 +1,25 @@
+ 8.66 KB
+
+import telnetlib
+import base64
+import cv2
+
+request =
+"""
+POST /test/ HTTP/1.0 From: frog@jmarshall.com
+User-Agent: HTTPTool/1.0
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 32"
+raw_byte = str.encode(raw)
+"""
+
+host = "127.0.0.1"
+port = 8083
+timeout = 100
+
+with telnetlib.Telnet(host, port, timeout) as session:
+    session.write(str.encode(request))
+    output = session.read_until(b"done", timeout)
+    session.close()
+    print(output)
+    print("Done")
