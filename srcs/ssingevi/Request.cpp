b98c59068a18e2191180d690c762fb04715a95f9
Aymeric SINGEVIN <ssingevi@e1r8p8.clusters.42paris.fr>
2022-08-31 17:38:47 +0200

    parsing 08/31

--- srcs/ssingevi/Request.cpp
+++ srcs/ssingevi/Request.cpp
@@ -1,253 +1,421 @@
 # include "Request.hpp"
 
-    va_list *Request::ret_copy(va_list src)
+Request::Request(const std::string& raw_request)
+{
+
+}
+va_list *Request::ret_copy(va_list src)
+{
+    va_list dst;
+    va_copy(dst,src);
+    return &dst;
+}
+
+void Request::n_star_m_or(int n, int m, ...)
+{
+    va_list arg;
+
+    va_start(arg, m);
+    n_star_m_or(n, m, arg);
+}
+
+void Request::n_star_m_or(int n, int m, va_list arg)
+{
+    std::string fct_ptr_tag = va_arg(arg, char *);
+
+    va_list *ap = ret_copy(arg);
+    int i = 0;
+    try
     {
-        va_list dst;
-        va_copy(dst,src);
-        return &dst;
+        _or(fct_ptr_tag, arg);
+        for (i = ((n > 0) ? 1 : 0); i != m; i++)
+        {
+            _or(fct_ptr_tag, *ret_copy(*ap));
+        }
     }
-
-    void Request::n_star_m_or(int n, int m, ...)
+    catch(const std::exception& e)
     {
-        va_list arg;
-
-        va_start(arg, m);
-        n_star_m_or(n, m, arg);
+        std::cerr << e.what() << '\n';
     }
+    
+}
 
-    void Request::n_star_m_or(int n, int m, va_list arg)
+void Request::n_star_m_and(int n, int m, ...)
+{
+    va_list arg;
+
+    va_start(arg, m);
+    n_star_m_and(n, m, arg);
+}
+
+void Request::n_star_m_and(int n, int m, va_list arg)
+{
+    std::string fct_ptr_tag = va_arg(arg, char *);
+
+    va_list *ap = ret_copy(arg);
+    int i = 0;
+
+    _and(fct_ptr_tag, arg);
+    for (i = ((n > 0) ? 1 : 0); i != m; i++)
     {
-        std::string fct_ptr_tag = va_arg(arg, char *);
-        va_list *ap = ret_copy(arg);
-        int i = 0;
+        _and(fct_ptr_tag, *ret_copy(*ap));
+    }
+}
 
+void Request::_or(const std::string &fct_ptr_tag, ...)
+{
+    va_list arg;
+    va_start(arg, fct_ptr_tag);
+    _or(fct_ptr_tag, arg);        
+}
+
+void Request::_and(const std::string &fct_ptr_tag, ...)
+{
+    va_list arg;
+    va_start(arg, fct_ptr_tag);
+    _and(fct_ptr_tag, arg);        
+}
+
+void Request::expand_va_arg(std::string::const_iterator &fct_it_tag, va_list arg)
+{
+    switch (*fct_it_tag)
+    {
+        case 's':
+            n_star_m(va_arg(arg, int), va_arg(arg, int), va_arg(arg, pf));
+            break;
+        case 'S':
+            switch (va_arg(arg, int))
+            {
+            case AND:
+                n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
+                break;
+            case OR:
+                n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
+            default:
+                throw std::invalid_argument("error _and wrong arg S");
+                break;
+            }
+            break;
+        case 'o':
+            _or(std::string(va_arg(arg, char *)) , arg);
+            break;
+        case 'a':
+            _and(std::string(va_arg(arg, char *)) , arg);
+            break;
+        case 'n':
+             (this->*va_arg(arg,void(Request::*)(void)))();
+            break;
+        case 'r' :
+            _range(va_arg(arg, char),va_arg(arg, char));
+            break;
+        case 'R' :
+            _is_str(va_arg(arg, char *));
+            break;
+        case 'c' :
+            _is_char(va_arg(arg, char));
+            break;
+        case 'C' :
+            _is_charset(std::string(va_arg(arg, char *)));
+            break;
+        default:
+            throw std::invalid_argument("error _and wrong arg");
+            break;
+    }
+}
+
+void Request::_and(const std::string &fct_ptr_tag, va_list arg)
+{
+    std::string::const_iterator start = fct_ptr_tag.begin();
+    std::string::const_iterator end = fct_ptr_tag.end();
+    
+    while (start != end)
+    {
+        expand_va_arg(start, arg);
+        start++;
+    }
+}
+
+void Request::_or(const std::string &fct_ptr_tag, va_list arg)
+{
+    std::string::const_iterator start = fct_ptr_tag.begin();
+    std::string::const_iterator end = fct_ptr_tag.end();
+
+    while (start != end)
+    {
         try
         {
-            _or(fct_ptr_tag, arg);
-            for (i = ((n > 0) ? 1 : 0); i != m; i++)
-            {
-                _or(fct_ptr_tag, *ret_copy(*ap));
-            }
+            expand_va_arg(start, arg);
+            return;
         }
         catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            if(start + 1 == end)
+                throw std::invalid_argument("error _or wrong arg");
+            (void)e;
         }
+        start++;    
+    }
+}
         
-    }
 
-    void Request::n_star_m_and(int n, int m, ...)
-    {
-        va_list arg;
+void Request::http_message()
+{
+    _and("nSns", &start_line, AND, STAR_NO_MIN, STAR_NO_MAX, "nn", &header_field, &CRLF, &CRLF, STAR_NO_MIN, STAR_NO_MAX, &message_body);
+}
 
-        va_start(arg, m);
-        n_star_m_and(n, m, arg);
-    }
+void Request::start_line()
+{
+    request_line();
+}
 
-    void Request::n_star_m_and(int n, int m, va_list arg)
-    {
-        std::string fct_ptr_tag = va_arg(arg, char *);
-        va_list *ap = ret_copy(arg);
-        int i = 0;
- 
-        _and(fct_ptr_tag, arg);
-        for (i = ((n > 0) ? 1 : 0); i != m; i++)
-        {
-            _and(fct_ptr_tag, *ret_copy(*ap));
-        }
-    }
+void Request::request_line()
+{
+    _and("nnnnnn", &method, &SP, &request_target, &SP, &http_version, &CRLF);
+}
 
-    void Request::_or(const std::string &fct_ptr_tag, ...)
-    {
-        va_list arg;
-        va_start(arg, fct_ptr_tag);
-        _or(fct_ptr_tag, arg);        
-    }
+void Request::method()
+{
+    _or("nnn", &_get, &_post, &_delete);
+}
 
-    void Request::_and(const std::string &fct_ptr_tag, ...)
-    {
-        va_list arg;
-        va_start(arg, fct_ptr_tag);
-        _and(fct_ptr_tag, arg);        
-    }
+void Request::_get()
+{
+    _is_str("GET");
+}
+void Request::_post()
+{
+    _is_str("POST");
+}
+void Request::_delete()
+{
+    _is_str("DELETE");
+}
 
-    void Request::_and(const std::string &fct_ptr_tag, va_list arg)
-    {
-        std::string::const_iterator start = fct_ptr_tag.begin();
-        std::string::const_iterator end = fct_ptr_tag.end();
-        
-        while (start != end)
-        {
-            switch (*start)
-            {
-            case 's':
-                n_star_m(va_arg(arg, int), va_arg(arg, int), va_arg(arg, pf));
-                break;
-            case 'S':
-                switch (va_arg(arg, int))
-                {
-                case AND:
-                    n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
-                    break;
-                case OR:
-                    n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
-                default:
-                    throw std::invalid_argument("error _and wrong arg S");
-                    break;
-                }
-                break;
+void Request::request_target()
+{
+    size_t old_head = _head;
 
-            case 'o':
-                _or(std::string(va_arg(arg, char *)), arg);
-                break;
-            case 'a':
-                _or(std::string(va_arg(arg, char *)), arg);
-                break;
-            case 'n':
-                (this->*va_arg(arg,pf))();
-                break;
-            case 'r' :
-                _range(va_arg(arg, char),va_arg(arg, char));
-                break;
-            case 'R' :
-                    is_str(va_arg(arg, char *);
-                break;
-            case 'c' :
-                _is_char(va_arg(arg, char));
-                break;
-            case 'C' :
-                _is_charset(std::string(va_arg(arg, char *)));
-                break;
-            default:
-                throw std::invalid_argument("error _and wrong arg");
-                break;
-            }
-            start++;
-        }
-        
-    }
+    _or("nnnn", &origin_form, &absolute_form, &authority_form, &asterisk_form);
+    _request_target = std::string(_raw_request, old_head, _head - old_head);
+}
 
+void Request::origin_form()
+{
+    _and("nS", &absolute_path, AND, STAR_NO_MIN, STAR_NO_MAX, "cn", '?', &query);
+}
+
+void Request::absolute_path()
+{
+    n_star_m_and(1, STAR_NO_MAX, "cn", '/', &segment);
+}
+
+void Request::segment()
+{
+    n_star_m(STAR_NO_MIN, STAR_NO_MAX, &PCHAR);
+}
+
+void Request::query()
+{
+    n_star_m_or(STAR_NO_MAX, STAR_NO_MIN, "ncc", &PCHAR, '/', '?');
+}
+
+void Request::LF()
+{
+    _is_char('\n');
+}
     
-    
+void Request::HTAB()
+{
+    _is_char('\t');
+}
 
-    void Request::_or(const std::string &fct_ptr_tag, va_list arg)
+void Request::SP()
+{
+    _is_char(' ');
+}
+
+void Request::DIGIT()
+{
+    _range('0', '9');
+}
+
+void Request::HEXDIG()
+{
+    _or("nr", &DIGIT, 'A', 'F');
+}
+
+void Request::BIT()
+{
+    _range('0', '1');
+}
+
+void Request::UPALPHA()
+{
+    _range('A', 'Z');
+}
+
+void Request::LOALPHA()
+{
+    _range('a', 'z');
+}
+
+void Request::ALPHA()
+{
+    _or("nn", &UPALPHA, &LOALPHA);
+}
+
+void Request::OCTET()
+{
+    _range(0x00, 0xFF);
+}
+
+void Request::segment()
+{
+    n_star_m_or(STAR_NO_MIN, STAR_NO_MAX, &PCHAR);
+}
+
+void Request::_is_str(std::string const &str)
+{
+    if(str.compare(_head, str.length(), _raw_request))
+        throw std::invalid_argument(str + " was expected");
+    _head += str.length();
+}
+
+void Request::_range(char start, char end)
+{
+    if (_raw_request[_head] < start || _raw_request[_head] > end)
+        throw std::invalid_argument("char between '" + std::string(&start, 1) + "' and '" + std::string(&end, 1) + "' was expected");
+    _head++;
+}
+
+void Request::_is_charset(std::string const &charset)
+{
+    std::string::const_iterator it = charset.begin();
+    for (; it != charset.end(); ++it)
     {
-        std::string::const_iterator start = fct_ptr_tag.begin();
-        std::string::const_iterator end = fct_ptr_tag.end();
-        pf fct;
-
-        while (start != end)
+        if (*it == _raw_request[_head])
         {
-            try
-            {
-                switch (*start)
-                {
-                case 's':
-                    n_star_m(va_arg(arg, int), va_arg(arg, int), va_arg(arg, pf));
-                    break;
-                case 'S':
-                    switch (va_arg(arg, int))
-                    {
-                    case AND:
-                        n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
-                        break;
-                    case OR:
-                        n_star_m_or(va_arg(arg, int),va_arg(arg, int), arg);
-                    default:
-                        throw std::invalid_argument("error _and wrong arg S");
-                        break;
-                    }
-                    break;
-                case 'o':
-                    _or(std::string(va_arg(arg, char *)), arg);
-                    break;
-                case 'a':
-                    _or(std::string(va_arg(arg, char *)), arg);
-                    break;
-                case 'n':
-                     (this->*va_arg(arg,pf))();
-                    break;
-                case 'r' :
-                    _range(va_arg(arg, char),va_arg(arg, char));
-                    break;
-                case 'R' :
-                    is_str(va_arg(arg, char *);
-                    break;
-                case 'c' :
-                    _is_char(va_arg(arg, char));
-                    break;
-                case 'C' :
-                    _is_charset(std::string(va_arg(arg, char *)));
-                    break;
-                default:
-                    throw std::invalid_argument("error _and wrong arg");
-                    break;
-                }
-                return;
-            }
-            catch (const std::invalid_argument& e)
-            {
-                (void)e;
-            }
-            start++;    
+            _head++;
+            return;
         }
-        throw std::invalid_argument("error _or wrong arg");
     }
+    throw std::invalid_argument(charset + " was expected");
+}
 
-    void Request::http_message()
-    {
-        _and("nSns", &start_line, AND, STAR_NO_MIN, STAR_NO_MAX,  "nn", &header_field, &CRLF, &CRLF, STAR_NO_MIN, STAR_NO_MAX, &message_body);
-    }
+void Request::_is_char(char c)
+{
+    if (_raw_request[_head] != c)
+        throw std::invalid_argument("char '" + std::string(&c, 1) + "' was expected");
+}
 
-    void Request::start_line(void)
-    {
-        request_line();
-    }
+void Request::tchar()
+{
+    _or("nnC", &DIGIT, &ALPHA, "!#$%&\'*+-.^_\\`|~");
+}
 
-    void Request::request_line()
-    {
-        _and("nnnnnn", &method, &SP, &request_target, &SP, &http_version, &CRLF);
-    }
+void Request::PCHAR()
+{
+    _or("nnncc", &unreserved, &pct_encoded, &sub_delims, ':', '@');
+}
 
-    void Request::method()
-    {
-        _or("nnn", &_get, &_post, &_delete);
-    }
+void Request::unreserved()
+{
+    _or("nnC", &ALPHA, &DIGIT, "-._~");
+}
 
-    void Request::_get()
-    {
-        is_str("GET");
-    }
-    void Request::_post()
-    {
-        is_str("POST");
-    }
-    void Request::_delete()
-    {
-        is_str("DELETE");
-    }
+void Request::pct_encoded()
+{
+    _and("cnn", '%', &HEXDIG, &HEXDIG);
+}
 
-    void Request::request_target()
-    {
-        _or("nnn", &origin_form, &absolute_form, &authority_form, &asterisk_form);
-    }
+void Request::sub_delims()
+{
+    _is_charset("!$&'()*+,;=");
+}
 
-    void Request::origin_form()
-    {
-        _and("nS", &absolute_path, AND, STAR_NO_MIN, STAR_NO_MAX, "cn", '?', &query);
-    }
+void Request::absolute_form()
+{
+    absolute_URI();
+}
 
-    void Request::absolute_path()
-    {
-        n_star_m_and(1, STAR_NO_MAX, "cn", '/', &segment);
-    }
+void Request::absolute_URI()
+{
+    _and("ncnS", &scheme, ':', &hier_part,  AND, STAR_NO_MIN, STAR_NO_MAX, "cn", '?', &query);
+}
 
-    void Request::segment()
-    {
-        n_star_m(STAR_NO_MIN, STAR_NO_MAX, &PCHAR);
-    }
+void Request::scheme()
+{
+    _and("nS", &ALPHA, OR, STAR_NO_MIN, STAR_NO_MAX, "nnC", &ALPHA, &DIGIT, "+-.");
+}
 
-    void Request::query()
-    {
-        n_star_m_or(STAR_NO_MAX, STAR_NO_MIN, "ncc", &PCHAR , '/', '?');
-    }
+
+// "//" [ authority / path_abempty / path_absolute / path_rootless ]
+void Request::hier_part()
+{
+    _and("RS", "//", OR, STAR_NO_MIN, STAR_NO_MAX , "nnnn", &authority, &path_abempty, &path_absolute, &path_rootless);
+}
+
+// authority = [ userinfo "@" ] host [ ":" port ]
+void Request::authority()
+{
+    _and("SnS", AND, STAR_NO_MIN, STAR_NO_MAX, "nc", &userinfo, '@', &host, AND, STAR_NO_MIN, STAR_NO_MAX, "cn", ':', &port);
+}
+
+
+// userinfo = *( unreserved / pct_encoded / sub_delims / ":" )
+void Request::userinfo()
+{
+    n_star_m_or(STAR_NO_MAX, STAR_NO_MIN, "nnnc", &unreserved, &pct_encoded, &sub_delims, ':');
+}
+
+void Request::host()
+{
+    _or("nnn", &IP_literal, &IPv4address, &reg_name);
+}
+
+void Request::IP_literal()
+{
+    _and("cSc", '[', &IPv6address, &IPvFuture, ']');
+}
+
+/*
+IPv6address =                            
+                                   6( h16 ":" ) ls32
+
+			/                       "::" 5( h16 ":" ) ls32
+
+			/ [               h16 ] "::" 4( h16 ":" ) ls32
+
+			/ [ *1( h16 ":" ) h16 ] "::" 3( h16 ":" ) ls32
+
+			/ [ *2( h16 ":" ) h16 ] "::" 2( h16 ":" ) ls32
+
+			/ [ *3( h16 ":" ) h16 ] "::"    h16 ":"   ls32
+
+			/ [ *4( h16 ":" ) h16 ] "::"              ls32
+			/ [ *5( h16 ":" ) h16 ] "::"              h16
+			/ [ *6( h16 ":" ) h16 ] "::"
+*/
+void Request::IPv6address()
+{
+    _or("aaaaaaa",
+        "Sn",       AND, 6, 6, "nc", &h16, ':', &ls32,
+        "RSn",      "::", AND, 5, 5, "nc", &h16, ':', &ls32,
+        "sRSn",     STAR_NO_MIN, 1, &h16, "::", AND, 4, 4, "nc", &h16, ':', &ls32,
+        "SRSn",     AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 1, "nc", &h16, ':', &h16, "::", AND, 3, 3, "nc", &h16, ':', &ls32,
+        "SRSn",     AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 2, "nc", &h16, ':', &h16, "::", AND, 2, 2, "nc", &h16, ':', &ls32,
+        "SRncn",    AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 3, "nc", &h16, ':', &h16, "::", &h16, ':', &ls32,
+        "SRn",      AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 4, "nc", &h16, ':', &h16, "::", &ls32,
+        "SRn",      AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 5, "nc", &h16, ':', &h16, "::", &h16,
+        "SR",       AND , STAR_NO_MIN, 1, "Sn", AND , STAR_NO_MIN, 5, "nc", &h16, ':', &h16, "::");
+}
+
+void Request::h16()
+{
+    n_star_m(1, 4, &HEXDIG);
+}
+
+void Request::ls32()
+{
+    _or("an", "ncn", &h16, ':', &h16, &IPv4address);
+}
--- srcs/ssingevi/Request.hpp
+++ srcs/ssingevi/Request.hpp
@@ -24,16 +24,18 @@ class Request
     std::string get_path();
     std::string get_host();
     int         get_method();
+    void        set_raw_request(const std::string& raw_request);
     
     private:
     
     typedef void (Request::*pf)(void);
 
 
-    int         _head;
+    size_t      _head;
     std::string _raw_request;
     int         _method;
     std::string _host;
+    std::string _request_target;
 
     /*
     fct_pointer_tag
@@ -42,45 +44,52 @@ class Request
     a   rule ' '
     n   normal fct_pointer
     r   _range
-    R   is_str
+    R   _is_str
     c   _is_char
     C   _is_charset
     */
 
-    va_list *ret_copy(va_list src);
-    // void expand_va_arg(va_list arg);
-    void n_star_m(int n, int m,void (Request::*fct)(void));  //s                                         // ✓
-    void n_star_m_or(int n, int m, ...);
+    va_list *ret_copy(va_list src);                                                             // ✓
+    void expand_va_arg(std::string::const_iterator &fct_it_tag, va_list arg);                   // ✓
+    void _range(char start, char end);                                                          // ✓
+    void _is_char(char c);                                                                      // ✓
+    void _is_charset(std::string const &charset);                                               // ✓
+    void _is_str(std::string const &str);                                                       // ✓
+    void n_star_m(int n, int m,void (Request::*fct)(void));  //s                                // ✓
+    void n_star_m_or(int n, int m, ...);                                                        // ✓
     void n_star_m_or(int n, int m, va_list arg);    //S                                         // ✓
-    void n_star_m_and(int n, int m, ...);
+    void n_star_m_and(int n, int m, ...);                                                       // ✓
     void n_star_m_and(int n, int m, va_list arg);   //S                                         // ✓
     void _or(const std::string &param, ... );       //o                                         // ✓
     void _or(const std::string &param, va_list arg);                                            // ✓
     void _and(const std::string &param, ... );      //a                                         // ✓
     void _and(const std::string &param, va_list arg);                                           // ✓                          
 
-    void DIGIT();
-    void UPALPHA();
-    void LOALPHA();
-    void ALPHA();
+    void DIGIT();               // ✓
+    void BIT();                 // ✓
+    void UPALPHA();             // ✓
+    void LOALPHA();             // ✓
+    void ALPHA();               // ✓  
     void CHAR();
-    void PCHAR(void);
+    void PCHAR();               // ✓
+        void unreserved();
+        void pct_encoded();
+        void sub_delims();
     void OCTET();    
-    void CR();
-    void LF();
-    void CRLF();
-    void SP();
-    void HTAB();
+    void CR();                  // ✓
+    void LF();                  // ✓
+    void CRLF();                // ✓
+    void SP();                  // ✓
+    void HTAB();                // ✓
     void WSP();
     void CTL();
     void OWS();
     void DQUOTE();
     void token();
+    void HEXDIG();  // ✓
+    void tchar();
+    void scheme();
     
-    void _range(char start, char end);
-    void _is_char(char c);
-    void _is_charset(std::string charset);
-    void _is_str(std::string str);
 
     void http_message();
         void start_line();
@@ -96,10 +105,21 @@ class Request
                         void query();
                     void absolute_form();
                         void absolute_URI();
+                        void hier_part();
+                            void path_abempty();
+                            void path_absolute();
+                            void path_rootless();
                     void authority_form();
                         void authority();
                             void userinfo();
                             void host();
+                                void IP_literal();
+                                    void IPv6address();
+                                        void h16();
+                                        void ls32();
+                                    void IPvFuture();
+                                void IPv4address();
+                                void reg_name();
                             void port();
                     void asterisk_form();
                 void http_version();
