faf10daac5a114b8a436b93564454f57547672e2
rballage <rballage@student.42.fr>
2022-09-09 16:40:40 +0200

    asd

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,11 +1,43 @@
-class client {
+typedef enum e_state {
+	ERROR, INCOMPLETE, READY, DONE
+} t_state;
+
+class Message
+{
+public:
+	t_state			state;
+	std::string		raw_data;
+	std::string		header;
+	std::string		body;
+	Client			*client;
+	Message			*ptr; // response || request
+	Info			info;
+
+	Message(Client c) :
+		client(c),
+		state(INCOMPLETE),
+		raw_data(NULL),
+		header(NULL),
+		body(NULL),
+		ptr(NULL),
+		info(NULL)
+	{};
+	virtual ~Message();
+
+};
+
+class Client {
 
 private:
 
 public:
-	int fd;
-	sockaddr_in address;
-
-	client (int file_descriptor, sockaddr_in addr) : fd(fd), address(addr) {};
-	virtual ~client ();
+	int				fd;
+	sockaddr_in		address;
+	Message			request;
+	Message			response;
+	Client(int file_descriptor, sockaddr_in addr) : fd(fd), address(addr) {};
+	virtual ~Client();
+	int receive(void) {
+		
+	}
 };
