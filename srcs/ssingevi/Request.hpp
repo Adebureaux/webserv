9d99f36ed902ce628d0dded0f7050bc47b8fa885
Aymeric SINGEVIN <ssingevi@e1r6p9.clusters.42paris.fr>
2022-09-13 12:18:11 +0200

    parsing 13/09

--- srcs/ssingevi/Request.hpp
+++ srcs/ssingevi/Request.hpp
@@ -7,101 +7,107 @@
 #include <map>
 #include <cstring> 
 
-#define GET			 0
-#define POST			1
-#define DELETE		  2
-#define NO_METHOD	  -1
+#define GET						 0
+#define POST					 1
+#define DELETE					 2
+#define NO_METHOD				-1
 
-#define OR			  1
-#define AND			 0
+#define OR						 1
+#define AND						 0
 
-#define STAR_NO_MIN	 0
-#define STAR_NO_MAX	-1
-#define EXECP		   std::invalid_argument(std::string(__FUNCTION__) + "/"+ std::string(e.what()))
-
-#define CATCH_VAR(X)	   _var_map[X] = std::string(_raw_request.begin()+ old_head,_raw_request.begin() + _head)
+#define STAR_NO_MIN				 0
+#define STAR_NO_MAX				-1
+#define EXECP					std::invalid_argument(std::string(__FUNCTION__) + "/"+ std::string(e.what()))
+#define CATCH_HEADER_VAR(X)		_header_var_map[X] = std::string(_raw_request.begin()+ old_head,_raw_request.begin() + _head)
+#define CATCH_VAR(X)			_var_map[X] = std::string(_raw_request.begin()+ old_head,_raw_request.begin() + _head)
 
 class Request
 {
 	public:
 	Request(const std::string& raw_request);
+	Request(const Request &cpy);
+	Request operator=(const Request &src);
 	~Request();
 
-	int							 get_method();
-	void							set_raw_request(const std::string& raw_request);
-	std::string const			   get_raw_request();
-	std::string const			   get_connection();
-	std::string const			   get_authority();
-	std::string const			   get_host();
-	std::string const			   get_request_target();
-	std::string const			   get_message_body();
-	std::pair<bool, std::string>	get_var_by_name(const std::string &name);
-	std::map<std::string, std::string>   get_map() const;
+	int							 		get_method();
+	void								set_raw_request(const std::string& raw_request);
+	std::string const					get_raw_request();
+	std::string const					get_connection();
+	std::string const					get_authority();
+	std::string const					get_host();
+	std::string const					get_request_target();
+	std::string const					get_message_body();
+	std::pair<bool, std::string>		get_var_by_name(const std::string &name);
+	std::map<std::string, std::string>	get_var_map() const;
+	std::map<std::string, std::string>	get_header_var_map() const;
+	bool								is_valid();
 
 	private:
 	
 	typedef void (Request::*pf)(void);
-	std::string _raw_request;
-	size_t	  _head;
-	int		 _method;
-	size_t	  _head_msg_body;
-	std::map<std::string, std::string> _var_map;
+	std::string							_raw_request;
+	size_t								_head;
+	int		 							_method;
+	size_t								_head_msg_body;
+	std::map<std::string, std::string>	_header_var_map;
+	std::map<std::string, std::string>	_var_map;
+	std::string 						_error_msg;
+	bool								_is_valid;
 
 	/*
 	fct_pointer_tag
 	s/S rule * 
-	o   rule |
-	a   rule ' '
-	n   normal fct_pointer
-	r   _range
-	R   _is_str
-	c   _is_char
-	C   _is_charset
+	o	rule |
+	a	rule ' '
+	n	normal fct_pointer
+	r	_range
+	R	_is_str
+	c	_is_char
+	C	_is_charset
 	*/
 
+	void catch_var_header_field(size_t old_head);
 	void ft_va_copy(va_list *dest, va_list *src);																		// ✓
-	void expand_va_arg(std::string::const_iterator &fct_it_tag, va_list *arg);							  // ✓
-	void finish_expand(std::string::const_iterator start, std::string::const_iterator end, va_list *arg);
-	void _range(char start, char end);																	  // ✓
-	void _is_char(char c);																				  // ✓
-	void _is_charset(std::string const &charset);														   // ✓
-	void _is_str(std::string const &str);																   // ✓
-	void n_star_m(int n, int m,void (Request::*fct)(void));  //s											// ✓
-	void n_star_m_or(int n, int m, ...);																	// ✓
-	void n_star_m_or(int n, int m, va_list *arg);	//S													// ✓
-	void n_star_m_and(int n, int m, ...);																   // ✓
-	void n_star_m_and(int n, int m, va_list *arg);   //S													// ✓
-	void _or(const std::string &param, ... );	   //o													 // ✓
-	void _or(const std::string &param, va_list *arg);													   // ✓
-	void _and(const std::string &param, ... );	  //a													 // ✓
-	void _and(const std::string &param, va_list *arg);													  // ✓						  
+	void expand_va_arg(std::string::const_iterator &fct_it_tag, va_list *arg);											// ✓
+	void finish_expand(std::string::const_iterator start, std::string::const_iterator end, va_list *arg);				// ✓
+	void _range(char start, char end);																					// ✓
+	void _is_char(char c);																								// ✓
+	void _is_charset(std::string const &charset);																		// ✓
+	void _is_str(std::string const &str);																				// ✓
+	void n_star_m(int n, int m,void (Request::*fct)(void));		// s													// ✓
+	void n_star_m_or(int n, int m, ...);																				// ✓
+	void n_star_m_or(int n, int m, va_list *arg);				// S													// ✓
+	void n_star_m_and(int n, int m, ...);																				// ✓
+	void n_star_m_and(int n, int m, va_list *arg);				// S													// ✓
+	void _or(const std::string &param, ... );					// o													// ✓
+	void _or(const std::string &param, va_list *arg);																	// ✓
+	void _and(const std::string &param, ... );					// a													// ✓
+	void _and(const std::string &param, va_list *arg);																	// ✓
 
-	void DIGIT();			   // ✓
-	void BIT();				 // ✓
-	void UPALPHA();			 // ✓
-	void LOALPHA();			 // ✓
-	void ALPHA();			   // ✓  
-	void CHAR();
-	void OCTET();	
-	void CR();				  // ✓
-	void LF();				  // ✓
+	void DIGIT();				// ✓
+	void BIT();					// ✓
+	void UPALPHA();				// ✓
+	void LOALPHA();				// ✓
+	void ALPHA();				// ✓	
+	void CHAR();				// ✓
+	void OCTET();				// ✓	
+	void CR();					// ✓
+	void LF();					// ✓
 	void CRLF();				// ✓
-	void SP();				  // ✓
+	void SP();					// ✓
 	void HTAB();				// ✓
-	void WSP();
-	void CTL();
-	void OWS();
-	void DQUOTE();
-	void token();
-	void HEXDIG();  // ✓
-	void tchar();
-	void scheme();
-	void PCHAR();			   // ✓
-		void unreserved();
-		void pct_encoded();
-		void sub_delims();
-	
-	void		catch_var_header_field(size_t old_head);
+	void WSP();					// ✓
+	void CTL();					// ✓
+	void OWS();					// ✓
+	void DQUOTE();				// ✓
+	void token();				// ✓
+	void HEXDIG();				// ✓
+	void tchar();				// ✓
+	void scheme();				// ✓
+	void PCHAR();				// ✓
+		void unreserved();		// ✓
+		void pct_encoded();		// ✓
+		void sub_delims();		// ✓
 
 	void http_message();
 		void start_line();
--- srcs/ssingevi/test.cpp
+++ srcs/ssingevi/test.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include "Request.hpp"
+#include "color_shell.hpp"
 
 // "POST / HTTP/1.1rnConnection: Keep-AlivernHost: localhost:1234\r\n\r\n"
 
@@ -24,12 +25,27 @@ int main(int ac, char **av)
 	  req = "GET /index.php?truc=machin&foo=bar&jean-michel=francis HTTP/1.1\r\nUser-Agent: custom-client\r\nHost: weebserv\r\nAccept-Language: en-US\r\nConnection: Keep-Alive\r\nContent-type: text/html\r\nContent-length: 16\r\n\r\n";
    }
    Request  re(req);
-   // std::cout << "end" << std::endl;
-   std::cout << std::endl;
+   std::cout << "req : ";
+   if (re.is_valid())
+      std::cout << C_G_GREEN << "OK";
+   else
+      std::cout << C_G_RED << "KO";
+   std::cout << C_RES << std::endl << std::endl;
    std::cout << "re.get_raw_request() :"<< std::endl << re.get_raw_request() << std::endl;
-   std::cout << "re.get_connection() :'" << re.get_connection() << "'"<< std::endl;
-   std::cout << "re.get_authority() :'" << re.get_authority() << "'"<< std::endl;
-   std::cout << "re.get_host() :'" << re.get_host() << "'" << std::endl;
-   std::cout << "re.get_request_target() :'" << re.get_request_target()<< "'" << std::endl;
-   // std::cout << re.get_message_body() << std::endl;   
+   std::map<std::string, std::string> req_map = re.get_var_map();
+   std::map<std::string, std::string>::iterator it = req_map.begin();
+   std::map<std::string, std::string>::iterator ite = req_map.end();
+   for(;it != ite; it++)
+   {
+      std::cout << it->first << " : \"" << it->second << "\"" << std::endl;
+   }
+
+   std::cout << std::endl;
+   std::map<std::string, std::string> req_head_map = re.get_header_var_map();
+   std::map<std::string, std::string>::iterator it_head = req_head_map.begin();
+   std::map<std::string, std::string>::iterator ite_head = req_head_map.end();
+   for(;it_head != ite_head; it_head++)
+   {
+      std::cout << it_head->first << " : \"" << it_head->second << "\"" << std::endl;
+   }
 }
