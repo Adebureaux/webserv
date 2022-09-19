3b4876876003e995b1212e886c82b6fd9cc1d165
rballage <rballage@student.42.fr>
2022-09-19 14:02:51 +0200

    sdf

--- autoindex.cpp
+++ autoindex.cpp
@@ -73,17 +73,19 @@ public:
 class Autoindex
 {
 private:
-	 const char *_html_start = "<!doctype html><html><header><style></style></header><body>";
+	 const char *_html_start = "<!doctype html><html><header><style>div {display: block;text-align: center;} a {vertical-align:middle;} img {vertical-align: bottom;width: 20px;}</style></header><body>";
 	 const char *_html_end = "</body></html>";
 	 // const char *_html_title_start = "<h3>";
 	 // const char *_html_title_end = "</h3>";
 	 const char *_html_a_start = "<div><a type=\"text/html\" href=\"";
 	 const char *_html_a_end = "</a></div>";
+	 const char *_html_file_icon = "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABIUlEQVRoge2ZSwrCMBBAn4Kix1UQd8UeQRfu9DJ6C0W8hmDRhS2E4qdxJhnQeTDQlCYzj5Y0TcFxnC6MgRI4AlfgFhkXYAn0cxcOj+L3EcW+iw0GEqVC4aYSxyD5DBhE9jeXCJ/54Rf9ze9EmFTaf/OknVxCU6AHrFvntiSW0BQAAwltAcgskUIAMkqkEoBMElKBT9Nwn8Szk1QgfBHOMZCQCixaY8TESlJ4g1RAshi8SApvkAoAjIACOBC/HH9Lr0Py9vydg845TT4wNHEBa1zAGhewxgWscQFrXMAaF7DGBaz5C4EqOP5mez2WMEf18qqaLgLn4HhCWokhMA3aJ41BJfs60ig0BDR/8sXEjsd2jAqSfZ2YuNY5Cs3iHeeXuQO5ISyjPwVqawAAAABJRU5ErkJggg==\">";
+	 const char *_html_folder_icon = "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABWUlEQVRoge2YXUrDUBBGjz8NdgFZhNAX11GXIb77+xhaV6ZY0DVoKYI7EPoiKfqQBibpbZJr7uQ+OAcGUnJvOF8ySWHAMAzDMHYZA3NgCeTAT0OtgVfgEjiKIVtnDCxolt5XT0A6vHKVOX+TL2sFTAa3FiyFzB0walmfUoSWrfYFnCs6NiJFEo99Uwrxcm8O3Aa364BsB18mFC3UpwVl5RQdMaN4N9UDAJwC3wFDlLUATrQDhH4C9co0A7jegRvPa9QZAffimm9dNvkGSIEHdr9CU0/ZfSRUb0orfR/zJ3AWSN7l5LXYtx7R+SdWDbAGXoAL4DCwuMupwnHLxgMVnYBo3bHBsACxsQCxsQCxsQCxsQCxsQCxcQXYiGOfsYoW0mFTP+kK8CGOr4gbIgGuxe9Vl00z9KYKg0wl+gx3NeuZjnMhtgsz4J328bpm5VuHzEfeMAzD+D/8AvAyR2dQGWtEAAAAAElFTkSuQmCC\">";
 
 	std::string _create_link(File file)
 	{
 		std::stringstream output;
-		output << _html_a_start << "/" << file.URI << "\">" << file.name << _html_a_end;
+		output << (file.type == DIRECTORY ? _html_folder_icon : _html_file_icon) << _html_a_start << "/" << file.URI << "\">" << file.name << _html_a_end;
 		return output.str();
 	};
 public:
