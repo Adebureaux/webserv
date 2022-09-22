58609bca7eac2085914830c574c5cb34dc7fe8b3
adeburea <adebureaux@gmail.com>
2022-09-22 11:18:11 +0200

    merge

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -4,20 +4,26 @@
 
 class Response
 {
+	typedef void(Response::*Method)(const Request&, const config_map::iterator&);
+
 	public:
-	void		create(const Request& request, const std::map<std::string, t_server_block>::iterator& config);
+	void		create(const Request& request, const config_map::iterator& config);
 	void		erase(void);
 	const void*	send(void) const;
 	size_t		get_size(void) const;
 
 	private:
-	void		_init_status_code(void) const;
-	void		_create_get(const Request& request, const std::map<std::string, t_server_block>::iterator& config);
+	void		_init_start_lines(void) const;
+	void		_create_get(const Request& request, const config_map::iterator& config);
+	void		_create_post(const Request& request, const config_map::iterator& config);
+	void		_create_delete(const Request& request, const config_map::iterator& config);
 	void		_generate_response(void);
+
+	// Method method[3] = { &Response::_create_get, &Response::_create_post, &Response::_create_delete };
 
 	private:
 	int			_status;
 	std::string _response;
 	std::string	_header;
-	std::string	_content;
+	std::string	_body;
 };
--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -1,16 +1,26 @@
 #include "Response.hpp"
 
-std::map<int, std::string> status_code;
+// https://www.rfc-editor.org/rfc/rfc7230.html#section-3 --> reference
+// HTTP-message   = start-line
+//                  *( header-field CRLF )
+//                  CRLF
+//                  [ message-body ]
 
-void Response::create(const Request& request, const std::map<std::string, t_server_block>::iterator& config)
+std::map<int, std::string> start_lines;
+
+void Response::create(const Request& request, const config_map::iterator& config)
 {
 	_status = 200;
-	if (!status_code.size())
-		_init_status_code();
+	if (!start_lines.size())
+		_init_start_lines();
 	if (!request.is_valid())
 		_status = 400;
 	else if (request.get_method() == GET)
 		_create_get(request, config);
+	else if (request.get_method() == POST)
+		_create_post(request, config);
+	else if (request.get_method() == DELETE)
+		_create_delete(request, config);
 	_generate_response();
 }
 
@@ -29,19 +39,19 @@ size_t Response::get_size(void) const
 	return (_response.size());
 }
 
-void Response::_init_status_code(void) const
+void Response::_init_start_lines(void) const
 {
-	status_code.insert(std::make_pair(100, "HTTP/1.1 100 Continue\n"));
-	status_code.insert(std::make_pair(200, "HTTP/1.1 200 OK\n"));
-	status_code.insert(std::make_pair(400, "HTTP/1.1 400 Bad Request\n"));
-	status_code.insert(std::make_pair(403, "HTTP/1.1 403 Forbidden\n"));
-	status_code.insert(std::make_pair(404, "HTTP/1.1 404 Not Found\n"));
-	status_code.insert(std::make_pair(405, "HTTP/1.1 405 Method Not Allowed\n"));
-	status_code.insert(std::make_pair(500, "HTTP/1.1 500 Internal Server Error\n"));
-	status_code.insert(std::make_pair(501, "HTTP/1.1 501 Not Implemented\n"));
+	start_lines.insert(std::make_pair(100, "HTTP/1.1 100 Continue\n"));
+	start_lines.insert(std::make_pair(200, "HTTP/1.1 200 OK\n"));
+	start_lines.insert(std::make_pair(400, "HTTP/1.1 400 Bad Request\n"));
+	start_lines.insert(std::make_pair(403, "HTTP/1.1 403 Forbidden\n"));
+	start_lines.insert(std::make_pair(404, "HTTP/1.1 404 Not Found\n"));
+	start_lines.insert(std::make_pair(405, "HTTP/1.1 405 Method Not Allowed\n"));
+	start_lines.insert(std::make_pair(500, "HTTP/1.1 500 Internal Server Error\n"));
+	start_lines.insert(std::make_pair(501, "HTTP/1.1 501 Not Implemented\n"));
 }
 
-void Response::_create_get(const Request& request, const std::map<std::string, t_server_block>::iterator& config)
+void Response::_create_get(const Request& request, const config_map::iterator& config)
 {
 	(void)config;
 	std::ifstream file(request.get_request_target().c_str()); // Integrate a root where to start finding
@@ -56,17 +66,29 @@ void Response::_create_get(const Request& request, const std::map<std::string, t
 	_header.append("Content-Type: text/html\n"); // SETUP CONTENT-TYPE HERE
 	_header.append("Content-Length: "); // SETUP CONTENT-LENGTH HERE
 	_header.append(content_size_stream.str());
-	_content.append(buffer);
-	std::cout << C_G_GREEN << _content << C_RES << std::endl;
-}	
+	_body.append(buffer);
+	std::cout << C_G_GREEN << _body << C_RES << std::endl;
+}
+
+void Response::_create_post(const Request& request, const config_map::iterator& config)
+{
+	(void)request;
+	(void)config;
+}
+
+void Response::_create_delete(const Request& request, const config_map::iterator& config)
+{
+	(void)request;
+	(void)config;
+}
 
 void Response::_generate_response(void)
 {
-	_response = status_code[_status];
+	_response = start_lines[_status];
 	_response.append(_header);
 	_header.erase();
 	_response.append("\r\n\r\n");
-	_response.append(_content);
-	_content.erase();
+	_response.append(_body);
+	_body.erase();
 	std::cout << C_G_GREEN << _response << C_RES;
 }
