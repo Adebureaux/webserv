e8797918a74a85f3af89c94bbbb7a94d35291149
Augustin Debureaux <adebureaux@gmail.com>
2022-10-14 23:10:36 +0200

    improve tester

--- telnet/telnet.py
+++ telnet/telnet.py
@@ -3,15 +3,25 @@ import sys
 
 if len(sys.argv) != 4:
     print("Missing argument")
-    sys.exit(-1)
+    sys.exit(1)
 
 host = sys.argv[1]
 port = sys.argv[2]
 
+# with open(sys.argv[3], 'r') as f:
+# 	print(f.read())
+
+request = []
+
+with open(sys.argv[3], 'r') as f:
+	for l in f:
+		request.append(l.rstrip("\n"))
+	f.close()
+
 with telnetlib.Telnet(host, port, 1) as session:
-	with open(sys.argv[3], 'r') as f:
-		session.write(f.readline().encode('ascii') + b"")
-		f.close()
+	for l in request:
+		session.write(l.encode('ascii') + b"")
+		session.write(bytes("\r\n", 'ascii'))
 	session.write(bytes("\r\n\r\n", 'ascii'))
 	output = session.read_all()
 	session.close()
