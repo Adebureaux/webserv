616fc63ead121218876ab375ed50467e2cb2e420
Augustin Debureaux <adebureaux@gmail.com>
2022-10-15 11:48:25 +0200

    minor change

--- telnet/telnet.py
+++ telnet/telnet.py
@@ -8,9 +8,6 @@ if len(sys.argv) != 4:
 host = sys.argv[1]
 port = sys.argv[2]
 
-# with open(sys.argv[3], 'r') as f:
-# 	print(f.read())
-
 request = []
 
 with open(sys.argv[3], 'r') as f:
@@ -22,7 +19,7 @@ with telnetlib.Telnet(host, port, 1) as session:
 	for l in request:
 		session.write(l.encode('ascii') + b"")
 		session.write(bytes("\r\n", 'ascii'))
-	session.write(bytes("\r\n\r\n", 'ascii'))
+	session.write(bytes("\r\n", 'ascii'))
 	output = session.read_some()
 	session.close()
 	start_line = output.decode().partition('\n')[0]
