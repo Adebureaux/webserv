0eb5119e374c0aceedb13e25fed4b2d3d897184a
rballage <rballage@student.42.fr>
2022-10-20 11:43:39 +0200

    POST form.php not working

--- html/sites/demo/hello.php
+++ html/sites/demo/hello.php
@@ -1,12 +1,3 @@
-<?php
-// This example illustrates the "HTTP/" special case
-// Better alternatives in typical use cases include:
-// 1. header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-//    (to override http status messages for clients that are still using HTTP/1.0)
-// 2. http_response_code(404); (to use the default message)
-header("HTTP/1.1 404 Not Found");
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
